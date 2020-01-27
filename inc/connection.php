23f15870f5d84c22ec72a843b09bb742aaa9f1ac
Atreids <56371059+atreids@users.noreply.github.com>
2020-01-27 16:21:18 +0000

    diverting to build db first

--- inc/connection.php
+++ inc/connection.php
@@ -1,16 +1,16 @@
 <?php
-#$serveraddress = "localhost";
-#$username = "root";
-#$dbpassword = "";
-#$dbname = "icinema";
-#$db = mysqli_connect($serveraddress, $username, $dbpassword, $dbname)
-#Or die ("failed to connect to databse");
+$serveraddress = "localhost";
+$username = "root";
+$dbpassword = "";
+$dbname = "tecbooks";
+$db = mysqli_connect($serveraddress, $username, $dbpassword, $dbname)
+Or die ("failed to connect to databse");
 
 #$serveraddress = "localhost";
 #$username = "HNDCSSA6";
 #$dbpassword = "HpDPLzWDuw";
 #$dbname = "HNDCSSA6";
 //todo
-$db = mysqli_connect($serveraddress, $username, $dbpassword, $dbname)
-Or die ("failed to connect to databse");
+#$db = mysqli_connect($serveraddress, $username, $dbpassword, $dbname)
+#Or die ("failed to connect to databse");
 ?>
