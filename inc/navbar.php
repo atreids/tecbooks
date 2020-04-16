7336879616187d7302b24b1ba6b054f9da4d0e7a
Atreids <56371059+atreids@users.noreply.github.com>
2020-04-16 16:11:10 +0100

    working on nav

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,20 +1,77 @@
-<nav class="nav-shadow">
-    <a href="index.php" class="logo">Tecbooks</a>
-    <input type="text" class="search" placeholder="Search..">
-    <a href="account.php" class="nav-link">Account</a>
-    <a href="contact.php" class="nav-link">Contact</a>
-    <a href="basket.php" class="nav-link">Cart</a>
-    <?php
-    if(isset($_SESSION['login'])){
-        echo '<a href="./inc/logout.php" class="nav-link">Logout</a>';
-    } else {
-        echo '<a href="login.php" class="nav-link">Login</a>
-        <a href="register.php" class="nav-link">Register</a>';
-    };?>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="./index.php">TecBooks</a>
+        </div>
 
-    <?php 
-        if(isset($_SESSION['login']) and $_SESSION['login'] == 'admin') {
-            echo '<a href="admin.php" class="nav-link">Admin</a>';
-        }
-        ?>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav nav-pills">
+                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                        aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                        aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
+<?php
+    #if(isset($_SESSION['login'])){
+     #   echo '<a href="./inc/logout.php" class="nav-link">Logout</a>';
+    #} else {
+    #    echo '<a href="login.php" class="nav-link">Login</a>
+    #    <a href="register.php" class="nav-link">Register</a>';
+    #};
+    ?>
+
+<?php 
+       # if(isset($_SESSION['login']) and $_SESSION['login'] == 'admin') {
+        #    echo '<a href="admin.php" class="nav-link">Admin</a>';
+       # }
+        ?>
+
+<ol class="breadcrumb">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Library</a></li>
+    <li class="active">Data</li>
+</ol>
--- index.php
+++ index.php
@@ -19,12 +19,62 @@ include("./inc/connection.php");
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <div class="container-fluid center">
+        <h1 class="title">TecBooks</h1>
+    </div>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="./index.php">HOME</a></li>
+                    <li><a href="#">BESTSELLERS</a></li>
+                    <li><a href="#">MATHEMATICS</a></li>
+                    <li><a href="#">COMPUTER SCIENCE</a></li>
+                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                            CATEGORIES</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">NEW</a></li>
+                            <li><a href="#">BIOLOGY</a></li>
+                            <li><a href="#">ARCHITECTURE</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="navbar-form navbar-right">
+                    <div class="form-group"><input type="text" class="form-control" placeholder="Search"></div>
+                    <button type="submit" class="btn btn-default">SEARCH</button>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php
+                        if(isset($_SESSION['login'])){
+                            echo '<li><a href="./inc/cart.php">Cart</a></li>';
+                            echo '<li><a href="./inc/logout.php">Logout</a></li>';
+                        } else {
+                            echo '<li><a href="login.php">Login</a></li>
+                            <li><a href="register.php">Register</a></li>';
+                        };
+                    ?>
+                </ul>
+            </div>
+            <!--End of navbar collapse-->
+        </div>
+    </nav>
+
+    <div class="parralax img-1"></div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="./node_modules/dist/jquery.min.js"></script>
     <!-- Bootstrap precompiled js -->
     <script src="./js/bootstrap.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
 </body>
 
 </html>
