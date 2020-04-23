6e53fe4764bafae8e43176a977aad6443d89b15c
Atreids <56371059+atreids@users.noreply.github.com>
2020-04-23 15:38:59 +0100

    Initialise rework

---
+++ inc/generic_header.php
@@ -0,0 +1,8 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+<!-- Generic Custom CSS -->
+<link rel="stylesheet" href="./scss/generic_style.css">
---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?phpinclude("./inc/generic_header.php");?>
+
+    <title>Tecbooks</title>
+</head>
+
+<body>
+    <?php include("./inc/nav.php");?>
+
+    <?phpinclude("./inc/generic_footer.php");?>
+</body>
+
+</html>
