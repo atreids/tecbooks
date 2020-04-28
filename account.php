54472c9708df1b4da22e63618f96679950ffed9e
Atreids <56371059+atreids@users.noreply.github.com>
2020-04-28 20:36:56 +0100

    started work on accounts page

---
+++ account.php
@@ -0,0 +1,88 @@
+<?php
+session_start();
+require("./php/connection.php");
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include("./inc/generic_header.php");?>
+
+    <title>Tecbooks</title>
+</head>
+
+<body class="d-flex flex-column">
+    <?php include("./inc/nav.php");?>
+    <div class="container-fluid divider"></div>
+    <div class="container margin-top">
+        <ul class="nav nav-tabs nav-fill">
+            <li class="nav-item">
+                <a class="nav-link active" data-toggle="tab" href="#details">Your
+                    Details</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#addresses">Your Addresses</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#advanced">Advanced</a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane fade show active" id="details">
+                <?php
+                    $sql = "SELECT * FROM Customers WHERE customer_id = ".$_SESSION['user_id']."";
+                    if(!mysqli_query($db,$sql)) {
+                        die ('Error: ' .mysqli_error($db));
+                    }else{
+                        $result = mysqli_query($db,$sql);
+                        $array = mysqli_fetch_array($result);
+                        $firstname = $array['firstname'];
+                        $lastname = $array['surname'];
+                        $email = $array['email'];
+                    }
+                ?>
+                <h3 class="margin-top">Your Details:</h3>
+                <p>Name: <?php echo $firstname." ".$lastname;?></p>
+                <p>Email: <?php echo $email ?></p>
+                <button class="btn btn-warning">Change Email</button><br>
+
+            </div>
+            <div class="tab-pane fade" id="addresses">
+                <?php
+                    $sql = "SELECT * FROM Customers WHERE customer_id = ".$_SESSION['user_id']."";
+                    if(!mysqli_query($db,$sql)) {
+                        die ('Error: ' .mysqli_error($db));
+                    }else{
+                        $result = mysqli_query($db,$sql);
+                        $array = mysqli_fetch_array($result);
+                        $firstname = $array['firstname'];
+                        $lastname = $array['surname'];
+                        $email = $array['email'];
+                    }
+                ?>
+
+
+            </div>
+            <div class="tab-pane fade" id="advanced">...</div>
+        </div>
+    </div>
+
+    <div class="container margin-top-lg margin-bottom">
+        <div class="row">
+            <div class="col-sm center-flex flex-column">
+                <h2><em>“The more that you read, the more things you will know. The more that you learn, the more places
+                        you’ll go.”</em> – Dr. Seuss</h2>
+                <p>Want to get even more reading in? Next time you are in store, talk to us about signing up to our
+                    monthly newsletter.
+                    <br>You'll receive all sorts of news on upcoming deals!</p>
+            </div>
+            <div class="col-sm">
+                <img src="./img/bookshelves.jpg" class="circle vsm-img">
+            </div>
+        </div>
+
+        <?php include("./inc/generic_footer.php");?>
+</body>
+
+</html>
--- inc/nav.php
+++ inc/nav.php
@@ -6,13 +6,13 @@
         <h1 class="logo">TecBooks</h1>
     </a>
 </div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav ml-4 mr-auto">
             <li class="nav-item">
                 <a class="nav-link" href="./index.php">HOME</a>
             </li>
@@ -24,7 +24,7 @@
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
-        <ul class="navbar-nav ml-auto flex-row-reverse">
+        <ul class="navbar-nav mr-4 ml-auto flex-row-reverse">
             <?php
                         if(isset($_SESSION['login'])){
                             $cart_number = 0;
@@ -35,7 +35,7 @@
                                 }
                             }
                             echo '<li class="nav-item"><a class="nav-link" href="./cart.php">CART('.$cart_number.')</a></li>';
-                            echo '<li class="nav-item"><a class="nav-link" href="./account.php"><svg class="bi bi-people-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            echo '<li class="nav-item"><a class="nav-link" href="./account.php"><svg class="bi bi-people-circle" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 008 15a6.987 6.987 0 005.468-2.63z"/>
                             <path fill-rule="evenodd" d="M8 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
                             <path fill-rule="evenodd" d="M8 1a7 7 0 100 14A7 7 0 008 1zM0 8a8 8 0 1116 0A8 8 0 010 8z" clip-rule="evenodd"/>
